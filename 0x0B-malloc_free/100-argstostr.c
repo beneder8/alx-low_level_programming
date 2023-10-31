833a95bcd3e2ca54c5cee80dabffb535b7ab4149
beneder8 <me@example.com>
2023-10-31 13:51:52 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr - concatenate command line arguments
- * @ac: number of arguments
- * @av: array of arguments
- * Return: concatenated string
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to a new string, or NULL if it fails
  */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;
-	char *str;
-
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	int i, j, len = 0, total_len = ac;
+
 	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-			l++;
-	}
-	l += ac;
+		if (av[i] == NULL)
+			return (NULL);
 
-	str = malloc(sizeof(char) * l + 1);
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+
+	char *str = malloc(len + total_len + 1);
+
 	if (str == NULL)
 		return (NULL);
 
+	len = 0;
+
 	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-		{
-			str[r] = av[i][n];
-			r++;
-		}
+		for (j = 0; av[i][j]; j++)
+			str[len++] = av[i][j];
 
-		if (i < ac - 1) /* add newline after each argument except the last one */
-		{
-			str[r] = '\n';
-			r++;
-		}
+		str[len++] = '\n';
 	}
+
+	str[len] = '\0';
 
 	return (str);
 }
